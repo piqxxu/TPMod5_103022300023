41b89041096ad66ab65d6b7b46dc593d15099165
Muhammad Azwa Radya Razadisya <137063182+piqxxu@users.noreply.github.com>
2025-03-15 21:58:19 +0700

    Menambahkan class HaloGeneric dengan method SapaUser

---
+++ TPMod5_103022300023/HaloGeneric.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPMod5_103022300023
+{
+    class HaloGeneric
+    {
+        public void SapaUser<X>(X nama)
+        {
+            Console.WriteLine($"Halo User, {nama}");
+        }
+    }
+}
--- TPMod5_103022300023/Program.cs
+++ TPMod5_103022300023/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using TPMod5_103022300023;
+
+class Program
+{
+    static void Main()
+    {
+        HaloGeneric welcome = new HaloGeneric();
+        welcome.SapaUser("Muhammad Azwa Radya Razadisya"); 
+
+        Console.ReadLine();
+    }
+}
